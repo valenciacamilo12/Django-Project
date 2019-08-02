012a1f78f766432ac35057d6344472f04b39f187
camilo valencia <camilo3341215@gmail.com>
2019-08-01 21:38:34 -0500

    Agregando los templates y lso archivos estaticos, ademos agrege el primer form

--- src/apps/productos/views.py
+++ src/apps/productos/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import CreateView,UpdateView,ListView,DeleteView
+from django.core.urlresolvers import reverse_lazy
+from apps.productos.models import Producto
 
-# Create your views here.
+
+class ProductCreate(CreateView):
+    model = Producto
+    form_class = ProductoForm
+    template_name = 'producto/producto_form.html'
+    success_url = reverse_lazy('producto:producto_crear')
